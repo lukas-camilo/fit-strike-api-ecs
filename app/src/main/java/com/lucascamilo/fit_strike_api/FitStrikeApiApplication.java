f269de9dbfca37de708fd00ef56301e61762b2c0
lukas-camilo <lukscamilo@gmail.com>
2025-05-06 15:19:25 -0300

    first commit

---
+++ app/src/main/java/com/lucascamilo/fit_strike_api/FitStrikeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucascamilo.fit_strike_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitStrikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitStrikeApiApplication.class, args);
+	}
+
+}
