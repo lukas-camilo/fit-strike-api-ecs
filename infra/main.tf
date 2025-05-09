fc074b4ce28f73c31b788ee3f5a8e9474f633e85
lukas-camilo <lukscamilo@gmail.com>
2025-05-08 23:33:04 -0300

    Ajustando roles

--- infra/main.tf
+++ infra/main.tf
@@ -226,12 +226,39 @@ resource "aws_iam_policy" "cloudwatch_logs_policy" {
   })
 }
 
+resource "aws_iam_policy" "dynamodb_access_policy" {
+  name = "DynamoDBAccessPolicy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Scan",
+          "dynamodb:Query"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_logs_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_dynamodb_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.dynamodb_access_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
